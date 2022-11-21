eba8afca22d20210c8eb23cb959320a3b7a175a4
arangobiophysics <34422667+arangobiophysics@users.noreply.github.com>
2022-11-21 15:34:03 -0600

    Update LipidHeatmap.py

--- analysis/LipidHeatmap.py
+++ analysis/LipidHeatmap.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import random
-for j in range(283,284):
+for j in range(283,330):
     b = []
     bx = []
     c = []
@@ -19,25 +19,73 @@ for j in range(283,284):
         r = r.reshape((int(r.shape[0]/3), 3))
         b = np.append(b, (np.min(r[0: , 0:1]), np.max(r[0: , 0:1]) ))
         c = np.append(c, (np.min(r[0: , 1:2]), np.max(r[0: , 1:2]) ))
-        ran = i + random.randint(280, 330)
+        ran = random.randint(280, 330)
         bx = np.append(bx, (np.min(r[0: , 0:1]), np.max(r[0: , 0:1]), ran ))
         cx = np.append(cx, (np.min(r[0: , 1:2]), np.max(r[0: , 1:2]), ran ))
         
     #d = np.where(b, )
     bx = bx.reshape((int(bx.shape[0]/3), 3))
-    bx = bx[0: , 2:3]
+    #bx = bx[0: , 2:3]
     cx = cx.reshape((int(cx.shape[0]/3), 3))
-    print(cx[0: , 2:3])
-    print(cx)
+    #
+    #print(cx)
+    #print(bx)
     #Working sort cx[cx[:, 0].argsort()]
     xsorted = bx[bx[:, 0].argsort()]
     ysorted = cx[cx[:, 0].argsort()]
-    
+    #print(xsorted, ysorted)
     #Making the bins
-    #bop = np.min(b), np.max(b)
-    #cop = np.min(c), np.max(c)
-    x = np.linspace(bop[0], bop[1], num=50)
-    w= len(x)
-    y = np.linspace(cop[0], cop[1], num=50)
+    bop = np.min(xsorted[0: , 0:1]), np.max(xsorted[0: , 1:2])
+    cop = np.min(ysorted[0: , 0:1]), np.max(ysorted[0: , 1:2])
+    x = np.linspace(bop[0], bop[1], num=100)
+    y = np.linspace(cop[0], cop[1], num=100)
+    #print(x)
+    w= len(cx[0: , 2:3])
+    v= len(x)
+    #print(v)
     
-    print(dx)
+
+        #list a
+    #FOR X
+    ll = []
+    for a in range(v):
+        l = []
+        for f in range(w):
+            k = xsorted[0: , 0:1][f]
+            h = xsorted[0: , 1:2][f]
+            temp = xsorted[0: , 2:3][f]
+            if k[0] <= y[a] <= h[0]:
+                l = np.append(l, temp)
+                #print(temp)
+            #else:
+             #   print("Nope")
+       
+        #print(np.mean(l))
+        ll = np.append(ll, np.mean(l))
+    #print(len(ll))
+    
+    #FOR Y
+    ly = []
+    for a in range(v):
+        l = []
+        for f in range(w):
+            k = ysorted[0: , 0:1][f]
+            h = ysorted[0: , 1:2][f]
+            temp = ysorted[0: , 2:3][f]
+            if k[0] <= y[a] <= h[0]:
+                l = np.append(l, temp)
+                #print(temp)
+            #else:
+             #   print("Nope")
+       
+        #print(np.mean(l))
+        ly = np.append(ly, np.mean(l))
+    #print(len(ly))
+    plt.xlim([-41, 41])
+    plt.ylim([-41, 41])
+    plt.subplot(111)
+    av = np.divide(np.add(ll, ly), 2)
+    plt.contourf(x,y,np.divide(np.outer(ll, ly), av))
+    #plt.contourf(x,y,np.divide(np.outer(ll, ly), av), levels=[280,305,330])
+    plt.colorbar()
+    plt.show()
